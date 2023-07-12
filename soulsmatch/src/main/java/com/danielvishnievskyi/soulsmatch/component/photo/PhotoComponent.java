dccd57de0a7f4e0ef7006476b20b35db37240f62
dv121ns <daniel.vishnievskyi@student.tuke.sk>
2023-07-12 23:34:17 +0200

    Components were changed to Utils

--- soulsmatch/src/main/java/com/danielvishnievskyi/soulsmatch/component/photo/PhotoComponent.java
+++
@@ -1,7 +0,0 @@
-package com.danielvishnievskyi.soulsmatch.component.photo;
-
-import com.danielvishnievskyi.soulsmatch.component.ComponentService;
-import com.danielvishnievskyi.soulsmatch.model.entity.Photo;
-
-public interface PhotoComponent extends ComponentService<Photo, String> {
-}
--- soulsmatch/src/main/java/com/danielvishnievskyi/soulsmatch/component/swipe/SwipeComponent.java
+++
@@ -1,8 +0,0 @@
-package com.danielvishnievskyi.soulsmatch.component.swipe;
-
-import com.danielvishnievskyi.soulsmatch.component.ComponentService;
-import com.danielvishnievskyi.soulsmatch.model.entity.Swipe;
-
-public interface SwipeComponent extends ComponentService<Swipe, String> {
-  void updateSwipedProfilesBySoul(String username, Long swipedProfileId);
-}
