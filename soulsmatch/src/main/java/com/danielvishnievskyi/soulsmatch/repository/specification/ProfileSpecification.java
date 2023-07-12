8a6a4021a401e1d6d5455167892b482e8bd6bbb1
dv121ns <daniel.vishnievskyi@student.tuke.sk>
2023-07-12 23:33:12 +0200

    Imports optimized

--- soulsmatch/src/main/java/com/danielvishnievskyi/soulsmatch/repository/specification/ProfileSpecification.java
+++ soulsmatch/src/main/java/com/danielvishnievskyi/soulsmatch/repository/specification/ProfileSpecification.java
@@ -3,7 +3,10 @@ package com.danielvishnievskyi.soulsmatch.repository.specification;
 import com.danielvishnievskyi.soulsmatch.model.entity.Profile;
 import com.danielvishnievskyi.soulsmatch.model.entity.Swipe;
 import com.danielvishnievskyi.soulsmatch.model.enums.Gender;
-import jakarta.persistence.criteria.*;
+import jakarta.persistence.criteria.Expression;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.Subquery;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.util.List;
@@ -26,16 +29,16 @@ public class ProfileSpecification {
     return (root, query, criteriaBuilder) -> root.get("soul").get("gender").in(preferredGenders);
   }
 
-  public static Specification<Profile> withCountry(String country) {
-    return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("soul").get("location").get("country").get("name"), country);
-  }
-
   public static Specification<Profile> withDistance(double lat1, double lon1) {
     return (root, query, criteriaBuilder) -> {
-      var lat2 = root.get("soul").get("location").get("latitude");
-      var lon2 = root.get("soul").get("location").get("longitude");
-      Expression<Double> distanceExpression = criteriaBuilder.function("getDistanceBetweenTwoPlacesByCoordinates", Double.class,
-        criteriaBuilder.literal(lat1), criteriaBuilder.literal(lon1), lat2, lon2);
+      Expression<Double> lat2 = root.get("location").get("latitude");
+      Expression<Double> lon2 = root.get("location").get("longitude");
+      Expression<Double> distanceExpression = criteriaBuilder.function(
+        "getDistanceBetweenTwoPlacesByCoordinates",
+        Double.class,
+        criteriaBuilder.literal(lat1), criteriaBuilder.literal(lon1),
+        lat2, lon2
+      );
       query.orderBy(criteriaBuilder.asc(distanceExpression));
       return criteriaBuilder.isTrue(criteriaBuilder.literal(true));
     };
